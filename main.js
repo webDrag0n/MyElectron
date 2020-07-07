e81eb8f3d76b32f848a928a42bc9d31e1dde99e2
webDrag0n <linzexinmasterchief@gmail.com>
2020-07-07 08:16:20 +0100

    simplify

--- main.js
+++ main.js
@@ -28,9 +28,7 @@ app.whenReady().then(createWindow)
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    
 })
 
 app.on('activate', () => {
